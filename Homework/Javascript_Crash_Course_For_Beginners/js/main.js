d9238ad180c11e5ea9ba87fc38719d274e4b792c
smcdev <smcdev@outlook.com>
2022-04-06 21:03:15 -0500

    Update main.js

--- Homework/Javascript_Crash_Course_For_Beginners/js/main.js
+++ Homework/Javascript_Crash_Course_For_Beginners/js/main.js
@@ -81,3 +81,67 @@ console.log(city);
 person.email = 'john@gmail.com';
 
 console.log(person);
+
+const todos = [
+  {
+    id: 1,
+    text: 'Take out trash',
+    isCompleted: true
+  },
+  {
+    id: 2,
+    text: 'Meeting with boss',
+    isCompleted: true
+  },
+  {
+    id: 3,
+    text: 'Dentist appt',
+    isCompleted: false
+  }
+];
+
+console.log(todos[1].text);
+
+const todoJSON = JSON.stringify(todos);
+
+console.log(todoJSON);
+
+//for loops
+
+for (let i = 0; i < 10; i++) {
+  console.log(`For Loop Number ${i}`);
+}
+
+//while loops
+
+let i = 0
+
+while (i < 10) {
+  console.log(`While Loop Number: ${i}`);
+  i++
+}
+
+for (let i = 0; i < todos.length; i++) {
+  console.log(todos[i].text);
+}
+
+for (let todo of todos) {
+  console.log(todo.text);
+}
+
+//ForEach, map, filter
+todos.forEach(function(todo) {
+  console.log(todo.text);
+});
+
+const todoText = todos.map(function(todo) {
+  return(todo.text);
+});
+
+console.log(todoText);
+
+const todoCompleted = todos.filter(function(todo) {
+  return todo.isCompleted === true;
+});
+
+console.log(todoCompleted);
