3a5152fd05828772a7e1e3db5af6aa70d80bb3f5
smcdev <smcdev@outlook.com>
2022-04-15 20:41:43 -0500

    Create main.js

---
+++ Homework/Javascript_Info/JSExpressions/js/main.js
@@ -0,0 +1,20 @@
+
+let age = prompt("What is your age?", 18);
+
+let welcome;
+
+if (age < 18) {
+
+  welcome = function() {
+    alert("Hello!");
+  };
+
+} else {
+
+  welcome = function() {
+    alert("Greetings!");
+  };
+
+}
+
+welcome(); // ok now
