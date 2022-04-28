1f8570d34ee1173c6418a0b7be988666538c3af2
smcdev <smcdev@outlook.com>
2022-04-28 18:24:35 -0500

    Update main.js

--- Classes/class30-materials/objects-espresso-machine/js/main.js
+++ Classes/class30-materials/objects-espresso-machine/js/main.js
@@ -1,1 +1,24 @@
 //Create an espresso machine constructor that makes machines with 4 properties and 3 methods
+
+class EspressoMachine {
+    constructor(color, make, model, price) {
+        this.color = color;
+        this.make = make;
+        this.model = model;
+        this.price = price;
+    }
+
+    turnOn(){
+        console.log('Good day, I am now on');
+    }
+
+    steam() {
+        console.log('ITTSSS SUMMERRR SALLLEEEE 50%%%%');
+    }
+
+    brew() {
+        console.log('Good stuff coming your way');
+    }
+}
+
+let gaggia = new EspressoMachine('red', 'Gaggia', 'Classic Pro', 400)
