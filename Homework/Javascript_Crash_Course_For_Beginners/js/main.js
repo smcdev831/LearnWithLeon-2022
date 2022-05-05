a63b34877d22f7a6a37675883598dadbf841c0f3
smcdev <smcdev@outlook.com>
2022-05-04 21:53:57 -0500

    Update main.js

--- Homework/Javascript_Crash_Course_For_Beginners/js/main.js
+++ Homework/Javascript_Crash_Course_For_Beginners/js/main.js
@@ -1,6 +1,6 @@
-console.log('Hello world');
-console.error('This is an error');
-console.warn('This is a warning');
+console.log("Hello world");
+console.error("This is an error");
+console.warn("This is a warning");
 
 //var, let, const -- let's not const use variables
 // String, Numbers, Boolean, null, undefined
@@ -15,13 +15,13 @@ console.warn('This is a warning');
 //
 // console.log(age);
 
-const name = 'John';
-const age = 30;
-const rating = 4.5;
-const isCool = true;
-const x = null;
-const y = undefined;
-let z
+let name = "John";
+let age = 30;
+let rating = 4.5;
+let isCool = true;
+let x = null;
+let y = undefined;
+let z;
 
 console.log(typeof name); //string
 console.log(typeof age); //number
@@ -32,72 +32,76 @@ console.log(typeof y); //undefined
 console.log(z); //undefined
 
 //concatenation
-console.log('My name is ' + name + ' and I am ' + age);
+console.log("My name is " + name + " and I am " + age);
 //Template string
 console.log(`My name is ${name} and I am ${age}`);
 
-const s = 'Hello World!';
+let s = "Hello World!";
 
 console.log(s.length);
 console.log(s.toUpperCase());
 console.log(s.toLowerCase());
 console.log(s.substring(0, 5)); //start and stop parameters
-console.log(s.split(''));
+console.log(s.split(""));
 
-const fruits = ['apples', 'oranges', 'pears']
+let fruits = ["apples", "oranges", "pears"];
 console.log(fruits);
 console.log(fruits[1]);
-fruits[3] = 'grapes';
-fruits.push('mangos');
-fruits.unshift('strawberries');
+fruits[3] = "grapes";
+fruits.push("mangos");
+fruits.unshift("strawberries");
 console.log(fruits);
 fruits.pop();
 console.log(fruits);
 console.log(Array.isArray(fruits));
-console.log(fruits.indexOf('oranges'));
+console.log(fruits.indexOf("oranges"));
 
 const person = {
-  firstName: 'John',
-  lastName: 'Doe',
+  firstName: "John",
+  lastName: "Doe",
   age: 30,
-  hobbies: ['music', 'movies', 'sports'],
+  hobbies: ["music", "movies", "sports"],
   address: {
-    street: '50 Main Street',
-    city: 'Boston',
-    state: 'MA'
-  }
-}
+    street: "50 Main Street",
+    city: "Boston",
+    state: "MA",
+  },
+};
 
 console.log(person);
 console.log(person.firstName, person.lastName);
 console.log(person.hobbies[1]);
 console.log(person.address.city);
 
-const {firstName, lastName, address: {city}} = person;
+const {
+  firstName,
+  lastName,
+  address: { city },
+} = person;
 
 console.log(firstName);
 console.log(city);
 
-person.email = 'john@gmail.com';
+person.email = "john@gmail.com";
 
 console.log(person);
 
 const todos = [
   {
     id: 1,
-    text: 'Take out trash',
-    isCompleted: true
+    text: "Take out trash",
+    isCompleted: true,
   },
   {
     id: 2,
-    text: 'Meeting with boss',
-    isCompleted: true
+    text: "Meeting with boss",
+    isCompleted: true,
   },
   {
     id: 3,
-    text: 'Dentist appt',
-    isCompleted: false
-  }
+    text: "Dentist appt",
+    isCompleted: false,
+  },
 ];
 
 console.log(todos[1].text);
@@ -114,11 +118,11 @@ for (let i = 0; i < 10; i++) {
 
 //while loops
 
-let i = 0
+let i = 0;
 
 while (i < 10) {
   console.log(`While Loop Number: ${i}`);
-  i++
+  i++;
 }
 
 for (let i = 0; i < todos.length; i++) {
@@ -130,18 +134,57 @@ for (let todo of todos) {
 }
 
 //ForEach, map, filter
-todos.forEach(function(todo) {
+todos.forEach(function (todo) {
   console.log(todo.text);
 });
 
-const todoText = todos.map(function(todo) {
-  return(todo.text);
+const todoText = todos.map(function (todo) {
+  return todo.text;
 });
 
 console.log(todoText);
 
-const todoCompleted = todos.filter(function(todo) {
+let todoCompleted = todos.filter(function (todo) {
   return todo.isCompleted === true;
 });
 
 console.log(todoCompleted);
+
+todoCompleted = todos
+  .filter(function (todo) {
+    return todo.isCompleted === true;
+  })
+  .map(function (todo) {
+    return todo.text;
+  });
+
+console.log(todoCompleted);
+
+//conditionals
+
+x = 6;
+y = 11;
+if (x === 10) {
+  console.log("X is 10");
+} else if (x > 5 && y > 10) {
+  console.log("X is more than 5, and y is more than 10");
+} else if (x > 5 || y > 10) {
+  console.log("X is more than 5, or y is more than 10");
+} else {
+  console.log("X is less than 10");
+}
+
+let color = x > 10 ? "red" : "blue";
+
+switch (color) {
+  case "red":
+    console.log("color is red");
+    break;
+  case "blue":
+    console.log("color is blue");
+    break;
+  default:
+    console.log();
+    "color is not red nor blue";
+    break;
+}
