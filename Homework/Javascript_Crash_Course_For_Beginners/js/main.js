24e255003036564a5932e7c69ccf4d4cfd7795bb
smcdev <smcdev@outlook.com>
2022-05-07 18:05:48 -0500

    Update main.js

--- Homework/Javascript_Crash_Course_For_Beginners/js/main.js
+++ Homework/Javascript_Crash_Course_For_Beginners/js/main.js
@@ -203,13 +203,15 @@ function Person(firstName, lastName, dob) {
   this.firstName = firstName;
   this.lastName = lastName;
   this.dob = new Date(dob);
-  this.getBirthYear = function () {
-    return this.dob.getFullYear();
-  };
-  this.getFullName = function () {
-    return `${this.firstName} ${this.lastName}`;
-  };
 }
+
+Person.prototype.getBirthYear = function () {
+  return this.dob.getFullYear();
+};
+
+Person.prototype.getFullName = function () {
+  return `${this.firstName} ${this.lastName}`;
+};
 
 //instantiate an object
 
@@ -220,3 +222,7 @@ console.log(person2.firstName);
 
 console.log(person1.getBirthYear());
 console.log(person1.getFullName());
+
+console.log(person1);
+
+console.log(person2.getFullName());
