1cc6fa6d574d1dec325349e2fa2588dd24a00246
smcdev <smcdev@outlook.com>
2022-05-17 20:31:50 -0500

    Update main.js

--- Classes/super-review 2/variable-fun-time/js/main.js
+++ Classes/super-review 2/variable-fun-time/js/main.js
@@ -1,16 +1,28 @@
 //--- Easy
 //create a variable and assign it a number
 
+let num = 6;
+
 //minus 10 from that number
 
+num -= 10;
+
 //print that number to the console
+
+console.log(num);
 
 //--- Medium
 //create a variable that holds a value from the input
 
+let inputInfo = Number(document.querySelector("#danceDanceRevolution").value);
+
 //add 25 to that number
 
+inputInfo += 25;
+
 //alert that number
+
+alert(inputInfo);
 
 //--- Hard
 //create a variable that holds the h1
