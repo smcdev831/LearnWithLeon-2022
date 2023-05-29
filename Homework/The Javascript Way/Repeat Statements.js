a77ce9bbb014581066f790b26030fc6ba7876c24
smcdev <smcdev@outlook.com>
2023-05-28 21:53:50 -0500

    Update Repeat Statements.js

--- Homework/The Javascript Way/Repeat Statements.js
+++ Homework/The Javascript Way/Repeat Statements.js
@@ -5,3 +5,14 @@ for (let i = 1; i <= 10; i++) {
     console.log(`${i} is odd`);
   }
 }
+
+//FizBuzz
+if (num / 15 === 0) {
+  return "Fizz-Buzz";
+} else if (num / 5 === 0) {
+  return "Buzz";
+} else if (num / 3 === 0) {
+  return "Fizz";
+} else {
+  return num;
+}
