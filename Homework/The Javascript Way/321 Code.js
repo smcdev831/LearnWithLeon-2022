5e0193304e82863c9c8c53d80cd738d8164e6602
smcdev <smcdev@outlook.com>
2023-05-28 20:24:07 -0500

    Create 321 Code.js

---
+++ Homework/The Javascript Way/321 Code.js
@@ -0,0 +1,11 @@
+console.log("Maria");
+console.log(42);
+
+console.log(6 + 3);
+console.log(6 - 3);
+console.log(6 * 3);
+console.log(6 / 3);
+
+console.log(4 + 5); //9
+console.log("4 + 5"); // 4 + 5
+console.log("4" + "5"); //45
