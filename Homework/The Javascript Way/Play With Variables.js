8a8996f7649b55ee511bb592e3be29628154137a
smcdev <smcdev@outlook.com>
2023-05-28 20:29:55 -0500

    Create Play With Variables.js

---
+++ Homework/The Javascript Way/Play With Variables.js
@@ -0,0 +1,23 @@
+let a = 2;
+a -= 1;
+a++;
+let b = 8;
+b += 2;
+const c = a + b * b; //102
+const d = a * b + b; //30
+const e = a * (b + b); //40
+const f = (a * b) / a; // 10
+const g = (b / a) * a; // 10
+console.log(a, b, c, d, e, f, g); //2, 10, 102, 30, 40, 10, 10
+
+console.log(userPrice * 0.206);
+
+let farenheit = c * (9 / 5) + 32;
+
+let number1 = 5;
+let number2 = 3;
+// TODO: type your code here (and nowhere else!)
+number1 = 3;
+number2 = 5;
+console.log(number1); // Should show 3
+console.log(number2); // Should show 5
